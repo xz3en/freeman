4a5217ca9f410ce8e06fe2f7478b2dbe45b54c50
xZen <86373768+xz3en@users.noreply.github.com>
2023-12-21 21:49:36 +0100

    Song command v0.0.1

---
+++ commands/song.ts
@@ -0,0 +1,35 @@
+import * as Eris from "eris"
+import BaseCommand from "../basecommand"
+
+export default class SongCommand extends BaseCommand {
+    public name: string = "song"
+    public description: string = "Group of subcommands for songs"
+    public options: Eris.ApplicationCommandOptions[] = [
+        {
+            name: "play",
+            description: "Play a song",
+            type: Eris.Constants.ApplicationCommandOptionTypes.SUB_COMMAND,
+            options: [
+                {
+                    name: "songname",
+                    description: "A name of a song you want to play",
+                    type: Eris.Constants.ApplicationCommandOptionTypes.STRING,
+                    required: true
+                }
+            ]
+        },
+        {
+            name: "stop",
+            description: "Stop song's playback",
+            type: Eris.Constants.ApplicationCommandOptionTypes.SUB_COMMAND
+        }
+    ]
+
+    constructor(public client: Eris.Client) {
+        super(client)
+    }
+
+    async execute(ctx: Eris.CommandInteraction) {
+        await ctx.createMessage("Pong!")
+    }
+}
